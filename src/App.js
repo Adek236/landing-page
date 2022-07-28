a3f8620b3753c0e8de273e2bd9ea7cc04a876364
unknown <adek236@gmail.com>
2022-07-28 15:21:23 +0200

    Added Header and Navigation components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import Layout from "./components/Layout/Layout";
+import Header from "./components/Header/Header";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <Layout>
-      Hello World
+      <Header>
+        <Navigation />
+      </Header>
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
+      Hello World <br />
     </Layout>
   );
 }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,78 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import DiamondIcon from "@mui/icons-material/Diamond";
+import PersonIcon from "@mui/icons-material/Person";
+import "./Navigation.css";
+
+function NavBar({ setShowModal }) {
+  return (
+    <div className="nav-bar">
+      <nav aria-label="Primary mobile" className="nav-mobile">
+        <div>
+          <a role="button" aria-label="Login" href="/">
+            <PersonIcon />
+          </a>
+        </div>
+        <div>
+          <a role="button" aria-label="Home page" href="/">
+            <DiamondIcon />
+          </a>
+        </div>
+        <div
+          role="button"
+          aria-label="Open modal"
+          tabIndex="0"
+          // onClick={() => setShowModal(true)}
+          // onKeyDown={(e) => {
+          //   if (e.key === "Enter") {
+          //     setShowModal(true);
+          //   }
+          // }}
+        >
+          <MenuIcon />
+        </div>
+      </nav>
+
+      <nav aria-label="Primary desktop" className="nav-desktop">
+        <div className="flex-center">
+          <a
+            role="button"
+            aria-label="Home page"
+            href="/"
+            className="flex-center"
+          >
+            <DiamondIcon />
+          </a>
+        </div>
+        <ul>
+          <li>
+            <a href="/" className="text-shadow">
+              HOME
+            </a>
+          </li>
+          <li>
+            <a href="/" className="text-shadow">
+              NEWS
+            </a>
+          </li>
+          <li>
+            <a href="/" className="text-shadow">
+              GUIDES
+            </a>
+          </li>
+          <li>
+            <a href="/" className="text-shadow">
+              OFFERS
+            </a>
+          </li>
+          <li>
+            <a href="/" className="text-shadow flex-center">
+              LOGIN <PersonIcon />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
