fda908dbb7878fa2f2cb2c5d7469468f4e6c74e6
unknown <adek236@gmail.com>
2022-08-04 09:56:42 +0200

    Added multimedia. Styled footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,33 +2,15 @@ import "./Footer.css";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import GoogleIcon from '@mui/icons-material/Google';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const Footer = ({ children }) => {
   return (
     <footer className="footer">
-      <div className="footer__socials">
-        <nav aria-label="Secondary">
-          <ul>
-            <li>
-              <a role="button" aria-label="Facebook" href="/">
-                <FacebookIcon />
-              </a>
-            </li>
-            <li>
-              <a role="button" aria-label="Instagram" href="/">
-                <InstagramIcon />
-              </a>
-            </li>
-            <li>
-              <a role="button" aria-label="Twitter" href="/">
-                <TwitterIcon />
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div>
-        <div>2022 ALL RIGHTS RESERVED.</div>
+      <div className="footer__nav">
+        <div className="footer__nav__copyright">2022 ALL RIGHTS RESERVED.</div>
         <nav aria-label="Third">
           <ul>
             <li>
@@ -49,13 +31,49 @@ const Footer = ({ children }) => {
               <a href="/">About</a>
             </li>
             <li>
-              <a href="/">Contact Us</a>
+              <a href="/">Contact</a>
             </li>
             <li>
               <a href="/">Support</a>
             </li>
             <li>
               <a href="/">Cookies</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="footer__socials flex-center">
+        <nav aria-label="Secondary">
+          <ul>
+            <li>
+              <a role="button" aria-label="Facebook" href="/">
+                <FacebookIcon />
+              </a>
+            </li>
+            <li>
+              <a role="button" aria-label="Instagram" href="/">
+                <InstagramIcon />
+              </a>
+            </li>
+            <li>
+              <a role="button" aria-label="Twitter" href="/">
+                <TwitterIcon />
+              </a>
+            </li>
+            <li>
+              <a role="button" aria-label="Twitter" href="/">
+                <GoogleIcon />
+              </a>
+            </li>
+            <li>
+              <a role="button" aria-label="Twitter" href="/">
+                <LinkedInIcon />
+              </a>
+            </li>
+            <li>
+              <a role="button" aria-label="Twitter" href="/">
+                <YouTubeIcon />
+              </a>
             </li>
           </ul>
         </nav>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,32 @@
 import "./Main.css";
+// import beachBackground from "../../assets/images/beach2.mp4";
 
 const Main = ({ children }) => {
   return (
     <main className="main">
-      <section>
+      <section 
+      // style={{ backgroundImage: `url(${beachBackground})` }}
+      >
+        
         <div className="main__section__container">
           <div className="main__section__container__desc">
-            <div className="main__section__container__desc__prev-title">Main content</div>
+            <div className="main__section__container__desc__prev-title">
+              Main content
+            </div>
             <h1>Main content</h1>
-            <div className="main__section__container__desc__slider">Main content</div>
+            <div className="main__section__container__desc__slider">
+              Main content
+            </div>
           </div>
           <div className="main__section__container__image">Graphics</div>
         </div>
       </section>
-
+      {/* <video className="videobg" autoPlay loop muted>
+          <source
+            src={beachBackground}
+            type="video/mp4"
+          />
+        </video> */}
       {children}
     </main>
   );
