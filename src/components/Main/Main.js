69f6037ea1023e9cdf7843c10f3c7d97390686d8
unknown <adek236@gmail.com>
2022-08-09 10:28:32 +0200

    Main functions v2

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,26 +7,39 @@ import beachBackground from "../../assets/images/beach3.jpg";
 const Main = ({ children }) => {
   const [slideNum, setSlideNum] = useState(1);
   const mainRef = useRef(null);
-  const { windowWidth, windowHeight } = useWindowWidth(mainRef);
+  const sectionRef = useRef(null);
+  const { windowWidth } = useWindowWidth(mainRef);
+  const { windowHeight } = useWindowWidth(sectionRef);
+  const isVideoWindow =
+    windowWidth >= 1200 && windowWidth / windowHeight <= 1.91 && windowWidth / windowHeight >= 1.6 ? true : false;
 
   useEffect(() => {
     let mainSlider = setInterval(() => {
-      console.log("tik tok");
       setSlideNum((prev) => {
         if (prev === 1) return (prev = 2);
         if (prev === 2) return (prev = 1);
       });
     }, 5000);
     return () => clearInterval(mainSlider);
-  });
+  }, []);
 
   return (
     <main ref={mainRef} className="main">
       {console.log("width = " + windowWidth, " height = ", windowHeight)}
       {console.log(windowWidth / windowHeight)}
-      <section style={{ backgroundImage: `url(${windowWidth >= 1200 && (windowWidth / windowHeight) <= 1.95 ? "" : beachBackground})` }}>
+      <section
+        ref={sectionRef}
+        style={{
+          backgroundImage: `url(${isVideoWindow ? "" : beachBackground})`,
+          boxShadow: `${
+            isVideoWindow ? "none" : "0px 1px 10px 0px rgba(0, 0, 0, 0.8)"
+          }`,
+        }}
+      >
         <video
-          style={{ display: `${windowWidth >= 1200 && (windowWidth / windowHeight) <= 1.95 ? "flex": "none"}` }}
+          style={{
+            display: `${isVideoWindow ? "flex" : "none"}`,
+          }}
           autoPlay
           loop
           muted
